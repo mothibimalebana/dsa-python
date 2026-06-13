223c5e72e74af03c5ca86192379f86bcf2640f94
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-13 18:04:39 +0200

    R-1.3

--- 1-python_primer/reinforcement.py
+++ 1-python_primer/reinforcement.py
@@ -8,7 +8,21 @@ def is_multiple(n, m):
 # returns True if k is even, and False otherwise. However, your function
 # cannot use the multiplication, modulo, or division operators.
 def is_even(k):
-    if k == 1:
-        return False
-    num = bin(k)
-    return True if (num[-1] == '0' and k != 1) else False
+    return (k & 1) == 0
+
+# R-1.3 Write a short Python function, minmax(data), that takes a sequence of
+# one or more numbers, and returns the smallest and largest numbers, in the
+# form of a tuple of length two. Do not use the built-in functions min or
+# max in implementing your solution
+def minmax(data):
+    
+    local_max = data[0]
+    local_min = data[0]
+
+    for num in data:
+        if num >  max:
+            max = num
+        elif num < min:
+            min = num
+
+    return(min, max)
