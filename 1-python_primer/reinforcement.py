9ac69351142d64e13b6d2fcaf29c7522ee99b1d8
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-13 16:52:50 +0200

    Complete R-1.1

--- 1-python_primer/reinforcement.py
+++ 1-python_primer/reinforcement.py
@@ -1,3 +1,8 @@
 # R-1.1 Write a short Python function, is multiple(n, m), that takes two integer
 # values and returns True if n is a multiple of m, that is, n = mi for some
 # integer i, and False otherwise
+def is_multiple(n, m):
+    try: 
+        return n % m  == 0
+    except ZeroDivisionError:
+        print("How Swae? You cannot divide by zero")
