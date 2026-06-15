623b7d8dba71144be3a048750aff7aff146729ff
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-15 10:44:08 +0200

    R-1.9

--- 1-python_primer/reinforcement.py
+++ 1-python_primer/reinforcement.py
@@ -71,6 +71,7 @@ def sum_of_squares_odd2(n):
 
 # R-1.9 What parameters should be sent to the range constructor, to produce a
 # range with values 50, 60, 70, 80?
+range(50, 90, 10)
 
 
 # R-1.10 What parameters should be sent to the range constructor, to produce a
