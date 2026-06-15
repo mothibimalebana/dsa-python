f3edd76ddb71a908e41bb8161135208a86c632b4
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-15 10:43:30 +0200

    R-1.8

--- 1-python_primer/reinforcement.py
+++ 1-python_primer/reinforcement.py
@@ -66,9 +66,12 @@ def sum_of_squares_odd2(n):
 # such as a string. If string s has length n, and expression s[k] is used for in-
 # dex −n ≤ k < 0, what is the equivalent index j ≥ 0 such that s[j] references
 # the same element?
+# my anser: 0 <= j < n
+
 
 # R-1.9 What parameters should be sent to the range constructor, to produce a
 # range with values 50, 60, 70, 80?
+
 
 # R-1.10 What parameters should be sent to the range constructor, to produce a
 # range with values 8, 6, 4, 2, 0, −2, −4, −6, −8?
