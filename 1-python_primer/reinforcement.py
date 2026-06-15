16af2d323ec76f4de5719c61520c3ec6ce48ca39
mothibi malebana <mothibi.mm12@gmail.com>
2026-06-15 10:52:26 +0200

    R-1.9

--- 1-python_primer/reinforcement.py
+++ 1-python_primer/reinforcement.py
@@ -76,9 +76,11 @@ range(50, 90, 10)
 
 # R-1.10 What parameters should be sent to the range constructor, to produce a
 # range with values 8, 6, 4, 2, 0, −2, −4, −6, −8?
+range(8, -9, -2)
 
 # R-1.11 Demonstrate how to use Python’s list comprehension syntax to produce
 # the list [1, 2, 4, 8, 16, 32, 64, 128, 256].
+[2**k for k in range(9)]
 
 # R-1.12 Python’s random module includes a function choice(data) that returns a
 # random element from a non-empty sequence. The random module in-
